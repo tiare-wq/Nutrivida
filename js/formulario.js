d020a6abeb2358adc0f220b2eca1704a60707600
Tiare <tiaresalazar.g@outlook.com>
2026-09-11 12:34:05 -0300

    feat: actualizar págine home.html y crear boton nutrivida, mejorar barra nav y hero

--- js/formulario.js
+++ js/formulario.js
@@ -4,15 +4,19 @@
 
 const tipoAtencion = document.getElementById("tipo-atencion-select");
 const planesServicios = document.getElementById("plan-servicio-select");
+const profesionales = document.getElementById("profesional-select");
 
 const opcionesPlanesServicios = planesServicios.options;
 
 tipoAtencion.onchange = function() {
 
     opcionesPlanesServicios.selectedIndex = 0;
+    profesionales.value = "";
 
-    if (tipoAtencion === "") {
+
+    if (tipoAtencion.value === "") {
         planesServicios.disabled = true;
+        profesionales.disabled = true;
         return;
     }
 
@@ -21,12 +25,51 @@ tipoAtencion.onchange = function() {
     const opcionSeleccionada = tipoAtencion.selectedOptions[0];
     
     for (const opc of opcionesPlanesServicios) {
+
+        if(opc.value == "") {
+            opc.hidden = false;
+            continue
+        }
+
         if (opc.classList.contains(opcionSeleccionada.value)) {
             opc.hidden = false;
         } else {
             opc.hidden = true;
         }
     }
+}
+
+// ======================================
+// OPCIONES DE SELECT PROFESIONAL
+// ======================================
+
+planesServicios.onchange = function() {
+
+    if (planesServicios.value === "") {
+        profesionales.disabled = true;
+        return
+    }
+
+    profesionales.disabled = false;
+
+    profesionales.value = "";
+
+    const opcionSeleccionada = planesServicios.selectedOptions[0].dataset.profesional.split(",");
+
+    for (const profesional of profesionales.options) {
+
+        if (profesional.value === "") {
+            profesional.hidden = false;
+            continue;
+        }
+
+        if (opcionSeleccionada.includes(profesional.className)) {
+            profesional.hidden = false;
+        } else {
+            profesional.hidden = true;
+        }
+    }
+
 }
 
 // ======================================
@@ -43,7 +86,7 @@ const campoFecha = document.getElementById("fecha");
 const campoHorario = document.getElementById("horario");
 
 // Patrones Regex
-const runPattern = /^\d{7,8}$/;
+const runPattern = /^\d{7,8}-[0-9kK]$/;
 const mailPattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/
 
 submit.onclick = function(event) {
@@ -57,13 +100,78 @@ submit.onclick = function(event) {
     const horario = campoHorario.value === "";
     const atencion = tipoAtencion.selectedOptions[0].value === "";
     const planServ = planesServicios.selectedOptions[0].value === "";
+    const profesional = profesionales.selectedOptions[0].value === "";
 
-    if (nombre || run || correo || fecha || horario || atencion || planServ) {
+    const errorNombre = document.getElementById("error-nombre");
+    const errorCorreo = document.getElementById("error-correo");
+    const errorRun = document.getElementById("error-run");
+    const errorAtencion = document.getElementById("error-atencion");
+    const errorPlanServ = document.getElementById("error-plan-serv");
+    const errorFecha = document.getElementById("error-fecha");
+    const errorHorario = document.getElementById("error-horario");
+    const errorProfesional = document.getElementById("error-profesional");
 
-        const mensajeError = document.getElementById("mensaje-error");
-        mensajeError.textContent = "Debe completar todos los campos"
-    } else {
-        mensajeForm.textContent = "";
+    errorNombre.textContent = "";
+    errorCorreo.textContent = "";
+    errorRun.textContent = "";
+    errorAtencion.textContent = "";
+    errorPlanServ.textContent = "";
+    errorFecha.textContent = "";
+    errorHorario.textContent = "";
+    errorProfesional.textContent = "";
+
+    let formularioValido = true;
+
+    if (nombre) {
+        errorNombre.textContent = "Introduzca su nombre";
+        formularioValido = false;
+    }
+
+    if (run) {
+        errorRun.textContent = "El formato del RUN no es válido. Por favor, introduzca su RUN sin puntos y con guión.";
+        formularioValido = false;
+    }
+
+    if (correo) {
+        errorCorreo.textContent = "El formato del correo no es válido.";
+        formularioValido = false;
+    }
+
+    if (atencion) {
+        errorAtencion.textContent = "Seleccione una atención.";
+        formularioValido = false;
+    }
+
+    if (planServ) {
+        errorPlanServ.textContent = "Seleccione un plan o servicio.";
+        formularioValido = false;
+    }
+
+    if (fecha) {
+        errorFecha.textContent = "Seleccione una fecha";
+        formularioValido = false;
+    }
+
+    if (horario) {
+        errorHorario.textContent = "Seleccione un horario";
+        formularioValido = false;
+    }
+
+    if (profesional) {
+        errorProfesional.textContent = "Seleccione un profesional";
+        formularioValido = false;
+    }
+
+    if (formularioValido) {
+
+        errorNombre.textContent = "";
+        errorCorreo.textContent = "";
+        errorRun.textContent = "";
+        errorAtencion.textContent = "";
+        errorPlanServ.textContent = "";
+        errorFecha.textContent = "";
+        errorHorario.textContent = "";
+        errorProfesional.textContent = "";
 
         //   1. Entregamos parámetros
         const parametros = new URLSearchParams();
