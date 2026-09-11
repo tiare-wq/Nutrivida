1875c5e89ea318ecad096e7cc42d5e62ab818f1a
Tiare <tiaresalazar.g@outlook.com>
2026-09-11 18:26:31 -0300

    feat: agregar calendario al formulario

--- js/formulario.js
+++ js/formulario.js
@@ -1,3 +1,7 @@
+function ocultarReservaHora() {
+        horario.classList.add("d-none");
+}
+
 // ======================================
 // OPCIONES DE SELECT PLANES-SERVICIOS
 // ======================================
@@ -5,6 +9,9 @@
 const tipoAtencion = document.getElementById("tipo-atencion-select");
 const planesServicios = document.getElementById("plan-servicio-select");
 const profesionales = document.getElementById("profesional-select");
+const horario = document.getElementById("reserva-hora");
+
+let campoDiaSeleccionado = null;
 
 const opcionesPlanesServicios = planesServicios.options;
 
@@ -12,13 +19,9 @@ tipoAtencion.onchange = function() {
 
     opcionesPlanesServicios.selectedIndex = 0;
     profesionales.value = "";
+    campoDiaSeleccionado = null;
 
-
-    if (tipoAtencion.value === "") {
-        planesServicios.disabled = true;
-        profesionales.disabled = true;
-        return;
-    }
+    ocultarReservaHora();
 
     planesServicios.disabled = false;
 
@@ -45,14 +48,12 @@ tipoAtencion.onchange = function() {
 
 planesServicios.onchange = function() {
 
-    if (planesServicios.value === "") {
-        profesionales.disabled = true;
-        return
-    }
+    ocultarReservaHora();
 
     profesionales.disabled = false;
 
     profesionales.value = "";
+    campoDiaSeleccionado = null;
 
     const opcionSeleccionada = planesServicios.selectedOptions[0].dataset.profesional.split(",");
 
@@ -73,17 +74,107 @@ planesServicios.onchange = function() {
 }
 
 // ======================================
+// SELECCIONAR HORARIO
+// ======================================
+
+const fechaActual = new Date();
+let mesActual =  fechaActual.getMonth();
+let anioActual = fechaActual.getFullYear();
+
+const cuerpoCalendario = document.getElementById("calendar-body");
+const campoMesActual = document.getElementById("current-month");
+
+const prevBtn = document.getElementById("prev-btn");
+const nextBtn = document.getElementById("next-btn");
+
+profesionales.onchange = function() {
+    horario.classList.remove("d-none");
+    renderCalendar();
+}
+
+prevBtn.onclick = function(event) {
+    event.preventDefault();
+
+    mesActual--;
+
+    if (mesActual < 0) {
+        mesActual = 11;
+        anioActual--;
+    }
+
+    renderCalendar();
+}
+
+nextBtn.onclick = function(event) {
+    event.preventDefault();
+
+    mesActual++;
+
+    if (mesActual > 11) {
+        mesActual = 0;
+        anioActual++;
+    }
+
+    renderCalendar();
+}
+
+function renderCalendar() {
+
+    const diasMes = new Date(anioActual, mesActual + 1, 0).getDate();
+    const primerDia = new Date(anioActual, mesActual, 1).getDay();
+
+    const posicionPrimerDia = (primerDia + 6) % 7;
+
+    const nombreMes = new Date(anioActual, mesActual).toLocaleDateString('es-CL', {month: "long", year: "numeric"});
+    campoMesActual.textContent = nombreMes[0].toUpperCase() + nombreMes.slice(1);
+
+    let dias = `<div class="week-day"><p><b>L</b></p></div>
+                <div class="week-day"><p><b>M</b></p></div>
+                <div class="week-day"><p><b>Mi</b></p></div>
+                <div class="week-day"><p><b>J</b></p></div>
+                <div class="week-day"><p><b>V</b></p></div>
+                <div class="week-day"><p><b>S</b></p></div>
+                <div class="week-day"><p><b>D</b></p></div>`;
+
+    for (let i = 0; i < posicionPrimerDia; i++) {
+        dias += `<div class="calendar-day empty"></div>`;
+    }
+
+    for(let i = 1; i <= diasMes; i++) {
+
+        dias += `
+                <div 
+                    class="calendar-day" 
+                    onclick="seleccionarDia(${i}, this)">
+                    ${i}
+                </div>
+            `;
+    }
+
+    cuerpoCalendario.innerHTML = dias;
+}
+
+function seleccionarDia(dia, elemento) {
+
+    document.querySelectorAll(".calendar-day")
+        .forEach(dia => dia.classList.remove("seleccionado"));
+
+    elemento.classList.add("seleccionado");
+    
+    campoDiaSeleccionado = new Date(anioActual, mesActual, dia);
+}
+
+// ======================================
 // VALIDACIÓN DE DATOS FORMULARIO
 // ======================================
 
-const submit = document.getElementById("enviar")
-const mensajeForm = document.getElementById("mensaje-form-invalido")
+const submit = document.getElementById("enviar");
+const mensajeForm = document.getElementById("mensaje-form-invalido");
 
 const campoNombre = document.getElementById("nombre");
 const campoRun = document.getElementById("run");
 const campoCorreo = document.getElementById("correo");
-const campoFecha = document.getElementById("fecha");
-const campoHorario = document.getElementById("horario");
+const campoHora = document.querySelector("input[name='hora']:checked");
 
 // Patrones Regex
 const runPattern = /^\d{7,8}-[0-9kK]$/;
@@ -91,24 +182,29 @@ const mailPattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/
 
 submit.onclick = function(event) {
 
-    event.preventDefault()
+    event.preventDefault();
 
     const nombre = campoNombre.value.trim() === "";
     const run = !runPattern.test(campoRun.value.trim());
     const correo = !mailPattern.test(campoCorreo.value.trim());
-    const fecha = campoFecha.value.trim() === "";
-    const horario = campoHorario.value === "";
+    const hora = campoHora === null;
     const atencion = tipoAtencion.selectedOptions[0].value === "";
     const planServ = planesServicios.selectedOptions[0].value === "";
     const profesional = profesionales.selectedOptions[0].value === "";
+    const hoy = new Date();
+    hoy.setHours(0, 0, 0, 0);
+
+    const fechaAnterior = campoDiaSeleccionado !== null &&
+                campoDiaSeleccionado < hoy;
+    const fecha = campoDiaSeleccionado === null;
 
     const errorNombre = document.getElementById("error-nombre");
     const errorCorreo = document.getElementById("error-correo");
     const errorRun = document.getElementById("error-run");
     const errorAtencion = document.getElementById("error-atencion");
-    const errorPlanServ = document.getElementById("error-plan-serv");
+    const errorPlanServ = document.getElementById("error-servicio");
     const errorFecha = document.getElementById("error-fecha");
-    const errorHorario = document.getElementById("error-horario");
+    const errorHora = document.getElementById("error-hora");
     const errorProfesional = document.getElementById("error-profesional");
 
     errorNombre.textContent = "";
@@ -117,7 +213,7 @@ submit.onclick = function(event) {
     errorAtencion.textContent = "";
     errorPlanServ.textContent = "";
     errorFecha.textContent = "";
-    errorHorario.textContent = "";
+    errorHora.textContent = "";
     errorProfesional.textContent = "";
 
     let formularioValido = true;
@@ -147,13 +243,16 @@ submit.onclick = function(event) {
         formularioValido = false;
     }
 
-    if (fecha) {
-        errorFecha.textContent = "Seleccione una fecha";
-        formularioValido = false;
+    if (fechaAnterior) {
+        errorFecha.textContent = "La fecha no puede ser anterior a la actual";
     }
 
-    if (horario) {
-        errorHorario.textContent = "Seleccione un horario";
+    if (fecha) {
+        errorFecha.textContent = "Seleccione una fecha";
+    }
+
+    if (hora) {
+        errorHora.textContent = "Seleccione un hora";
         formularioValido = false;
     }
 
@@ -169,16 +268,15 @@ submit.onclick = function(event) {
         errorRun.textContent = "";
         errorAtencion.textContent = "";
         errorPlanServ.textContent = "";
-        errorFecha.textContent = "";
-        errorHorario.textContent = "";
+        errorHora.textContent = "";
         errorProfesional.textContent = "";
 
         //   1. Entregamos parámetros
         const parametros = new URLSearchParams();
         parametros.append("nombre", campoNombre.value.trim());
         parametros.append("run", campoRun.value.trim())
-        parametros.append("fecha", campoFecha.value);
-        parametros.append("horario", campoHorario.value);
+        parametros.append("fecha", campoDiaSeleccionado.toISOString());
+        parametros.append("hora", campoHora);
         parametros.append("atencion", tipoAtencion.selectedOptions[0].value);
         parametros.append("planServ", planesServicios.selectedOptions[0].value);
         parametros.append("precio", planesServicios.selectedOptions[0].dataset.precio);
